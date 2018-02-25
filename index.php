6af9b596ded6a6b44f15cf60577fa6439b62c07e
Swashata Ghosh <swashata@wpquark.com>
2018-02-25 11:42:47 +0530

    Fix Cherry-Fix Conflict

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 echo "<h1>Hello Unlimited Multiverse!</h1>";
 echo "<p>We are Earth 101</p>";
+echo "<p>Hello People (Stupid)</p>";
+echo "<p>Hello Dogs</p>";
 echo "<p>This is line1</p>";
 echo "<p>This is Line2</p>";
 echo "<p>This is Line3</p>";
